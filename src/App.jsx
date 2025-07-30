f6c1380982a7b63e4b21fdd477e8a86a62b6acd1
Dhan <edan.belgica@cvsu.edu.ph>
2025-07-30 15:21:18 +0800

    chore: add dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import OrgChart from "./components/OrgChart";
 
 import PersonDetail from "./components/PersonDetail"; // Create this next
 import NotFound from "./components/NotFound";
+import Dashboard from "./components/Dashboard";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<OrgChart />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/orgchart" element={<OrgChart />} />
         <Route path="/home" element={<Header />} /> {/* 👈 Add this line */}
         <Route path="/person/:id" element={<PersonDetail />} />
         <Route path="*" element={<NotFound />} />
